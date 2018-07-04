3128f12fc291e318d4b26749c69a1f604fdff1bf
yanggc02 <yanggc02@yanggc02>
2018-07-04 17:37:07 +0800

    any object can use jdk proxy.

---
+++ myproxy/src/cn/sky/myproxy/test2/ObjectTest.java
@@ -0,0 +1,25 @@
+package cn.sky.myproxy.test2;
+
+import org.junit.Test;
+
+public class ObjectTest {
+	@Test
+	public void test1() {
+		Object o = new Object();
+		o = ProxyFactory.getInstance().createProxy(o);
+		o.getClass();//native method cannot use proxy
+		o.equals("");
+		o.hashCode();
+		o.toString();
+		Class<?>[] cs = o.getClass().getInterfaces();
+		System.out.println(cs.length);
+		//Object是没有实现任何接口的，但是也能使用jdk的动态代理。
+	}
+	@Test
+	public void test2() throws Throwable {
+		//换言之，任何对象都可以使用jdk的动态代理，只是，代理的方法只有Object的这几个方法，没什么暖用
+		Object t = new ObjectTest();
+		t = ProxyFactory.getInstance().createProxy(t);
+		t.toString();
+	}
+}
